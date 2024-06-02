c0893ce194d57ca0cccbc47bff325a15f13beb4e
Anthony de Cruz <anthonydecruz@proton.me>
2024-06-02 18:27:32 +0100

    enable sending of sms messages

--- node/app.js
+++ node/app.js
@@ -5,6 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var databaseController = require("./controllers/databaseController");
+var smsController = require("./controllers/smsController");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
