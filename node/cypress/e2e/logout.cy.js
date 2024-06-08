3fd5ecfeadfffb5e97bdb49b2830c5a03ce3f741
Anthony de Cruz <anthonydecruz@proton.me>
2024-06-08 19:50:12 +0100

    cleaned up code

--- node/cypress/e2e/logout.cy.js
+++ node/cypress/e2e/logout.cy.js
@@ -2,22 +2,21 @@ describe("logout functionality", () => {
     beforeEach(() => {});
 
     it("redirects to login when not logged in", () => {
+        // Setup
         cy.clearCookie("authToken");
+
+        // Logout
         cy.visit("http://localhost:8080/logout");
+
+        // Assert
         cy.url().should("include", "/login");
     });
 
     it("can logout when logged in", () => {
-        // Log in
-        // cy.clearCookie("authToken");
-        // cy.visit("http://localhost:8080/login");
-        // cy.get("#username").type("staff1");
-        // cy.get("#password").type("password");
-        // cy.get('[type="submit"]').click();
-
+        // Setup
         cy.login("staff1", "password");
 
-        // Log out
+        // Logout
         cy.visit("http://localhost:8080/logout");
         cy.get('[type="submit"]').click();
 
