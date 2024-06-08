990efa3c9414b91479dcb96ba5f2304576b3b2f0
Anthony de Cruz <anthonydecruz@proton.me>
2024-06-08 19:24:10 +0100

    hotfix: fixed e2e test bug

--- node/cypress/e2e/login.cy.js
+++ node/cypress/e2e/login.cy.js
@@ -4,7 +4,7 @@ describe("login functionality", () => {
         cy.visit("http://localhost:8080/login");
     });
 
-    afterAll(() => {
+    afterEach(() => {
         cy.clearCookie("authToken");
     });
 
